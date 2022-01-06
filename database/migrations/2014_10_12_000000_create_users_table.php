769e8f56cbd20d9a0c5c848cae64a570fadd293e
GoBigWeb <devteam@gobigweb.com>
2022-01-06 22:42:41 +0530

    fix db migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->foreignId('status_id')->default(1)->constrained('statuses');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
